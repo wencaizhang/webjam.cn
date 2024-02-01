f40503fb4216b1018192775ddcd7fd1ef498df4c
wencaizhang <1052642137@qq.com>
2024-02-01 09:35:11 +0800

    feat(project): add tooltip

--- src/modules/projects/components/ProjectLink.tsx
+++ src/modules/projects/components/ProjectLink.tsx
@@ -2,11 +2,13 @@ import Link from 'next/link';
 import { ReactNode } from 'react';
 import { BsGithub as GithubIcon } from 'react-icons/bs';
 import { FiExternalLink as LinkIcon } from 'react-icons/fi';
+import Tooltip from '@/common/components/elements/Tooltip';
 
 interface LinkComponentProps {
   url: string;
   text: string;
   icon?: ReactNode;
+  disabled?: boolean;
 }
 
 interface ProjectLinkProps {
@@ -33,22 +35,38 @@ const ProjectLink = ({ title, link_github, link_demo }: ProjectLinkProps) => {
 
   return (
     <div className='flex gap-4'>
-      {link_github && (
+      {link_github ? (
         <LinkComponent
           url={link_github}
           text='Source Code'
-          icon={<GithubIcon size={22} />}
+          icon={<GithubIcon size={16} />}
         />
+      ) : (
+        <Tooltip title='代码暂未公开'>
+          <div className='flex gap-2 items-center font-medium text-gray-400 dark:text-gray-500'>
+            <GithubIcon size={16} />
+            <span>Source Code</span>
+          </div>
+        </Tooltip>
       )}
       {link_github && link_demo && (
         <span className='text-neutral-400 dark:text-neutral-600'>|</span>
       )}
-      {link_demo && (
-        <LinkComponent
-          url={link_demo}
-          text='Live Demo'
-          icon={<LinkIcon size={22} />}
-        />
+      {link_demo ? (
+        <Tooltip title='查看演示'>
+          <LinkComponent
+            url={link_demo}
+            text='Live Demo'
+            icon={<LinkIcon size={16} />}
+          />
+        </Tooltip>
+      ) : (
+        <Tooltip title='暂无演示'>
+          <div className='flex gap-2 items-center font-medium text-gray-400 dark:text-gray-500'>
+            <LinkIcon size={16} />
+            <span>Live Demo</span>
+          </div>
+        </Tooltip>
       )}
     </div>
   );
