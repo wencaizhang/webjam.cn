082347a59779a28f2b9870872688bd118eb33072
wencaizhang <1052642137@qq.com>
2024-02-01 16:18:44 +0800

    refactor(snippets): refact snippets list page

---
+++ src/common/types/StackIcon.tsx
@@ -0,0 +1,147 @@
+import gitlab from '~public/icons/stack/gitlab.svg';
+import lerna from '~public/icons/stack/lerna.svg';
+import prisma from '~public/icons/stack/prisma.svg';
+import ts from '~public/icons/stack/ts.svg';
+
+import Pinia from '~public/icons/stack/Pinia.svg';
+import css3 from '~public/icons/stack/css3.svg';
+import go from '~public/icons/stack/go.svg';
+import less from '~public/icons/stack/less.svg';
+import ps from '~public/icons/stack/ps.svg';
+import vant from '~public/icons/stack/vant.png';
+
+import actionvue from '~public/icons/stack/actionvue.png';
+import golang from '~public/icons/stack/golang.svg';
+import mysql from '~public/icons/stack/mysql.svg';
+import psv from '~public/icons/stack/psv.png';
+import vim from '~public/icons/stack/vim.svg';
+
+import angular from '~public/icons/stack/angular.svg';
+import docker from '~public/icons/stack/docker.svg';
+import html from '~public/icons/stack/html.svg';
+import nestjs from '~public/icons/stack/nestjs.svg';
+import python from '~public/icons/stack/python.svg';
+import vite from '~public/icons/stack/vite.svg';
+
+import antdv from '~public/icons/stack/antdv.png';
+import eleplus from '~public/icons/stack/ele+.png';
+import nextjs from '~public/icons/stack/nextjs.svg';
+import raspberrypi from '~public/icons/stack/raspberry-pi.svg';
+import vscode from '~public/icons/stack/vscode.svg';
+
+import axios from '~public/icons/stack/axios.webp';
+import ele from '~public/icons/stack/ele.png';
+import java from '~public/icons/stack/java.svg';
+import nginx from '~public/icons/stack/nginx.svg';
+import reactnative from '~public/icons/stack/react-native.svg';
+import vue from '~public/icons/stack/vue.svg';
+
+import babel from '~public/icons/stack/babel.svg';
+import es from '~public/icons/stack/es.svg';
+import nodejs from '~public/icons/stack/nodejs.svg';
+import react from '~public/icons/stack/react.svg';
+import webpack from '~public/icons/stack/webpack.svg';
+
+import caddy from '~public/icons/stack/caddy.png';
+import firefox from '~public/icons/stack/firefox.svg';
+import jenkins from '~public/icons/stack/jenkins.svg';
+import npm from '~public/icons/stack/npm.svg';
+import sass from '~public/icons/stack/sass.svg';
+import wordpress from '~public/icons/stack/wordpress.svg';
+
+import chrome from '~public/icons/stack/chrome.svg';
+import flutter from '~public/icons/stack/flutter.svg';
+import js from '~public/icons/stack/javascript.svg';
+import php from '~public/icons/stack/php.svg';
+import shopify from '~public/icons/stack/shopify.svg';
+import yarn from '~public/icons/stack/yarn.svg';
+
+import git from '~public/icons/stack/git.svg';
+import k8s from '~public/icons/stack/k8s.svg';
+import postcss from '~public/icons/stack/postcss.svg';
+import tailwindcss from '~public/icons/stack/tailwind-css.svg';
+
+export let StackIconsMap = {
+  gitlab,
+  lerna,
+  prisma,
+  ts,
+
+  Pinia,
+  css3,
+  go,
+  less,
+  ps,
+  vant,
+
+  actionvue,
+  golang,
+  mysql,
+  psv,
+  vim,
+
+  angular,
+  docker,
+  html,
+  nestjs,
+  python,
+  vite,
+
+  antdv,
+  eleplus,
+  nextjs,
+  raspberrypi,
+  vscode,
+
+  axios,
+  ele,
+  java,
+  nginx,
+  reactnative,
+  vue,
+
+  babel,
+  es,
+  nodejs,
+  react,
+  webpack,
+
+  caddy,
+  firefox,
+  jenkins,
+  npm,
+  sass,
+  wordpress,
+
+  chrome,
+  flutter,
+  js,
+  php,
+  shopify,
+  yarn,
+
+  git,
+  k8s,
+  postcss,
+  tailwindcss,
+};
+
+export function BrandIcon(props: {
+  type: keyof typeof StackIconsMap;
+  className?: string;
+}) {
+  let { type, className } = props;
+  let Icon = StackIconsMap[type];
+  if (!Icon) return <div>Missing icon for {type}</div>;
+
+  return (
+    <div
+      className={className || 'h-16 w-16 lg:h-14 lg:w-14 xl:h-20 xl:w-20'}
+      style={{
+        backgroundImage: `url(${Icon.src})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'contain',
+      }}
+    />
+  );
+}
--- src/pages/snippets/index.tsx
+++ src/pages/snippets/index.tsx
@@ -5,8 +5,9 @@ import { NextSeo } from 'next-seo';
 import Container from '@/common/components/elements/Container';
 import PageHeading from '@/common/components/elements/PageHeading';
 import { getCollection } from '@/common/libs/mdx';
-import { MdxFileContentProps } from '@/common/types/learn';
+import { MdxFileContentProps } from '@/common/types/snippets';
 import { siteMetadata } from '@/contents/siteMetadata';
+import { BrandIcon } from '@/common/types/StackIcon';
 
 interface ContentPageProps {
   contentList: MdxFileContentProps[];
@@ -18,13 +19,13 @@ const SnippetsContentPage: NextPage<ContentPageProps> = ({ contentList }) => {
   }
 
   const title = '代码段';
-  const description = '这里是代码片段';
+  const description = '这里是代码片段，可以直接过来拷贝使用';
 
   const canonicalUrl = `${siteMetadata.siteUrl}/snippets`;
 
-  const activeClasses = `flex justify-between w-full font-sora items-center gap-2 py-2 px-4 rounded-lg group
+  const activeClasses = `flex justify-between w-full font-sora items-center gap-2 rounded-lg group
     text-neutral-700 dark:text-neutral-400 hover:text-neutral-900 hover:dark:text-neutral-300 
-    hover:dark:lg:bg-neutral-800 hover:dark:!text-neutral-300 hover:lg:bg-neutral-200 hover:lg:rounded-lg lg:hover:scale-105 lg:transition-all lg:duration-300
+     hover:dark:!text-neutral-300 hover:lg:rounded-lg lg:transition-all lg:duration-300
   `;
   return (
     <>
@@ -44,16 +45,31 @@ const SnippetsContentPage: NextPage<ContentPageProps> = ({ contentList }) => {
       />
       <Container data-aos='fade-up'>
         <PageHeading title={title} description={description} />
-        {contentList.map((item) => (
-          <Link
-            className={activeClasses}
-            href={`/snippets/${item.slug}`}
-            key={item.slug}
-          >
-            <span> {item.frontMatter.title} </span>
-            <span> {item.frontMatter.date} </span>
-          </Link>
-        ))}
+        <div className='grid-cols-2 gap-6 lg:grid'>
+          {contentList.map((item) => (
+            <Link
+              className={activeClasses}
+              href={`/snippets/${item.slug}`}
+              key={item.slug}
+              title={item.frontMatter.description || item.frontMatter.title}
+            >
+              <div className='h-full w-full mb-4 p-3 lg:p-4 gap-4 flex cursor-pointer rounded-lg border border-transparent shadow-intense hover:shadow-nextjs dark:shadow-intense-dark dark:hover:shadow-nextjs-dark lg:mb-0'>
+                <div className='flex items-center justify-center'>
+                  <BrandIcon type={item.frontMatter.type} />
+                </div>
+                <div className='overflow-hidden space-y-2'>
+                  <h3 className='overflow-hidden overflow-ellipsis whitespace-nowrap text-base font-bold leading-8 tracking-tight lg:text-lg'>
+                    {' '}
+                    {item.frontMatter.title}{' '}
+                  </h3>
+                  <p className='text-gray-600 dark:text-gray-400 text-sm lg:text-base'>
+                    {item.frontMatter.description}
+                  </p>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
       </Container>
     </>
   );
